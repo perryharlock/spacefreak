24713ea7677debe3ab18808aaf2c8fa6e0370e1b
Perry Harlock <perry@hirespace.com>
2014-05-30 14:09:20 +0100

    Added javascript for questions

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+	var totalScore = 0;
+	var totalQuestions = 0;
+
+	// Set all radio buttons to empty
+	$("input[type='radio']").prop('checked', false);
+
+	// If clicking on a radio button hide the error message if it is there
+	$("input[type='radio']").click(function(){
+		$('[data-role=error]').addClass('hidden');
+	});
+
+	// On clicking onto next question
+	$('[data-role=submit-answer]').click(function(event){
+		// event.preventDefault();
+		var thisButton = $(this);
+		var thisAnswer = $("input[type='radio']:checked");
+
+		// If they have answered
+		if (thisAnswer.length > 0) {
+			thisAnswer = thisAnswer.val();
+			var correctAnswer = answers()[thisButton.data('question') - 1];
+
+			console.log(totalScore);
+			if (thisAnswer === correctAnswer) {
+				if (localStorage.totalScore) {
+					totalScore = localStorage.totalScore;
+				}
+				totalScore++;
+				localStorage.setItem('totalScore', totalScore);
+			}
+			if (localStorage.totalQuestions) {
+				totalQuestions = localStorage.totalQuestions;
+			}
+			totalQuestions ++;
+			localStorage.setItem('totalQuestions', totalQuestions);
+		}
+		// Otherwise show an error
+		else {
+			event.preventDefault();
+			$('[data-role=error]').removeClass('hidden');
+		}
+	});
+
+	//Play again and reset
+	$('[data-role=reset]').click(function(event){
+		localStorage.setItem('totalScore', 0);
+		localStorage.setItem('totalQuestions', 0);
+	});
+
+	// The answers!
+	function answers() {
+		return [
+		"2",
+		"3",
+		"2",
+		"2",
+		"1",
+		"1",
+		"2",
+		"3"
+		];
+	}
+
+});
